ecbe16a5b6e370d10842f02b4771f3472a496a27
alimuratkuslu <alimuratkuslu@gmail.com>
2024-10-17 17:07:21 +0300

    Added JpaRepositories

--- src/main/java/vending_machine/DemoAppApplication.java
+++ src/main/java/vending_machine/DemoAppApplication.java
@@ -3,9 +3,11 @@ package vending_machine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import vending_machine.config.ProductConfig;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "vending_machine.adapter.out.persistence")
 @Import({
 		ProductConfig.class
 })
